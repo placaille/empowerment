9ccbeefd9f9f6c14b59b23ee80bc707dff16e43a
Philippe Lacaille <philippe.lacaille@gmail.com>
2019-02-19 13:06:10 -0500

    magic ssh forwarding home

--- scripts/tensorboard_slurm.sh
+++ scripts/tensorboard_slurm.sh
@@ -3,12 +3,14 @@
 # https://gist.github.com/taylorpaul/250ee3ed2524e8c28ee7c58ed656a5b9
 
 usage() {
-»       echo " Usage : $0 [-l <log_dir>]"
+»       echo " Usage : $0 [-l <log_dir>] [-f forward-home]"
 }
 
 LOG_DIR=""
-while getopts l: flag; do
+FWD_HOME=false
+while getopts l:f flag; do
   case $flag in
+    f) FWD_HOME=true ;;
     l) LOG_DIR=$OPTARG ;;
     *) usage; exit;;
     ?) usage; exit;;
@@ -23,4 +25,10 @@ echo ipnport=$ipnport
 ipnip=$(hostname -i)
 echo ipnip=$ipnip
 
+if [ "$FWD_HOME" = true ] ; then
+  echo Forwarding port $ipnport to home:12900
+  ssh -fNR :12900:localhost:${ipnport} home
+fi
+
+# launch tensorboard
 tensorboard --logdir="${LOG_DIR}" --port=$ipnport
